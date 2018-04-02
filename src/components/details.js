53e68927c9caed574193185071f9573a3b73d1b4
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-02 10:36:47 +0530

    single input and details main component

--- src/components/details.js
+++ src/components/details.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Select = (props) => (
-	<div className="form-group">
+const details = (props) => (
+	<div>
+		<div>Your ownerName  ::  {props.ownerName}</div><br/><br/>
+		<div>Your Email  ::  {props.Email}</div><br/><br/>
+		<div>Your password  ::  {props.password}</div><br/><br/>
+		<div>Your phoneNumber  ::  {props.phoneNumber}</div><br/><br/>
+		<div>Your Country  ::  {props.ownerAgeRangeSelection}</div><br/><br/>
+		
 	
+		
+
 	</div>
 );
 
-Select.propTypes = {
-	};
 
-export default Select;
+export default details;
